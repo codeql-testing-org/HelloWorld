9e8685a4e0f8de9ef4609929e31b9559d7af19ad
Lei Yu <leiyu@redhat.com>
2016-04-19 14:04:29 +0100

    change it version two now

--- src/main/java/org/larinia/test/App.java
+++ src/main/java/org/larinia/test/App.java
@@ -4,10 +4,15 @@ package org.larinia.test;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public void testMethod () {
+
+      System.out.println("Hello World Twice!");
     }
 }
