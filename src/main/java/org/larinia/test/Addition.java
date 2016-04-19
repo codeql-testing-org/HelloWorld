bd2f8a353f5899c1cb8dd7704adc9b6294349eb8
Lei Yu <leiyu@redhat.com>
2016-04-19 15:37:07 +0100

    added a lot of line to this

--- src/main/java/org/larinia/test/Addition.java
+++ src/main/java/org/larinia/test/Addition.java
@@ -4,7 +4,13 @@ public class Addition {
 
 	public static void main(String[] args) {
 		System.out.println("type something here");
+		Addition add = new Addition();
+		add.someMethod();
 
+	}
+
+	public void someMethod() {
+		System.out.println("something else here ");
 	}
 
 }
--- src/main/java/org/larinia/test/App.java
+++ src/main/java/org/larinia/test/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        App app = new App();
+        app.testMethod();
     }
 
     public void testMethod () {
